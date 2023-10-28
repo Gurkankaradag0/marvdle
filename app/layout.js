8b4cd94fe8e8177014519fb54e92878fdaf11c76
Gürkan Karadağ <gurkan.karadag.1907@gmail.com>
2023-10-28 20:57:43 +0300

    feat: add add block

--- app/layout.js
+++ app/layout.js
@@ -36,6 +36,18 @@ const RootLayout = ({ children }) => {
                     crossOrigin='anonymous'
                     strategy='afterInteractive'
                 />
+                <Script
+                    id='Add-block-1'
+                    async
+                    src='https://fundingchoicesmessages.google.com/i/pub-8917872308902699?ers=1'
+                    nonce='WCxhoLicszDOyx099MS8fg'
+                />
+                <Script
+                    id='Add-block-2'
+                    nonce='WCxhoLicszDOyx099MS8fg'
+                >
+                    {`(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`}
+                </Script>
                 <StoreProvider>
                     <TooltipProvider>
                         <HomeLayoutContainer>{children}</HomeLayoutContainer>
